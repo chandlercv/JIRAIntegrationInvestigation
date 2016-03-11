b4582cc8256d1f862fd98a3b1f954b5b120dac06
chandlercv <charles.chandler@assemblesystems.com>
2016-03-10 21:55:57 -0600

    No issue required

---
+++ Rules Panel Borked in all browsers.mm
@@ -0,0 +1,44 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1443126457217" ID="ID_565683254" MODIFIED="1443126514842" TEXT="Rules Panel Borked in all browsers">
+<node CREATED="1443126515555" ID="ID_1869658872" MODIFIED="1443126558897" POSITION="right" TEXT="caused by constraints not being initialized when translating filters to rules"/>
+<node CREATED="1443127725211" ID="ID_1440639431" MODIFIED="1443127763833" POSITION="right" TEXT="currently translateFiltersToRules will never create a constraint">
+<node CREATED="1443127764331" ID="ID_76427401" MODIFIED="1443128793713" TEXT="confirmed this with Hala"/>
+</node>
+<node CREATED="1443128307849" ID="ID_552800483" MODIFIED="1443128333956" POSITION="right" TEXT="verify no errors in elmah or console while testing"/>
+<node CREATED="1443127989163" ID="ID_1435044818" MODIFIED="1443127991310" POSITION="right" TEXT="Chrome">
+<node CREATED="1443127780537" FOLDED="true" ID="ID_116191923" MODIFIED="1443143106032" TEXT="Opening views that have been copied to FederatedViews but not persisted">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1443127940203" ID="ID_570447900" MODIFIED="1443127949159" TEXT="unedited"/>
+<node CREATED="1443127949651" ID="ID_3265338" MODIFIED="1443127971031" TEXT="edited to add constraint when needed"/>
+</node>
+<node CREATED="1443127818177" ID="ID_438748745" MODIFIED="1443128266440" TEXT="Opening views that have been copied to FederatedViews and persisted">
+<node CREATED="1443127940203" ID="ID_1473804228" MODIFIED="1443130729016" TEXT="unedited">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1443127949651" ID="ID_1245511557" MODIFIED="1443127971031" TEXT="edited to add constraint when needed"/>
+</node>
+</node>
+<node CREATED="1443128001943" ID="ID_198269640" MODIFIED="1443128003674" POSITION="right" TEXT="FF">
+<node CREATED="1443127780537" ID="ID_1884652083" MODIFIED="1443127817618" TEXT="Opening views that have been copied to FederatedViews but not persisted">
+<node CREATED="1443127940203" ID="ID_271948499" MODIFIED="1443146452946" TEXT="unedited">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1443127949651" ID="ID_810190140" MODIFIED="1443146455638" TEXT="edited to add constraint when needed">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1443128014175" ID="ID_1100683459" MODIFIED="1443128016784" POSITION="right" TEXT="IE">
+<node CREATED="1443127780537" ID="ID_329779237" MODIFIED="1443127817618" TEXT="Opening views that have been copied to FederatedViews but not persisted">
+<node CREATED="1443127940203" ID="ID_1101806269" MODIFIED="1443146736403" TEXT="unedited">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1443127949651" ID="ID_1504436603" MODIFIED="1443146739056" TEXT="edited to add constraint when needed">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</map>
